460afed00cd43089b56f2110134a4fa45fb76348
NBRET-TOUCH-WASH <90608656+NBRET-TOUCH-WASH@users.noreply.github.com>
2022-12-11 21:05:51 +0100

    Started player object creation in main gameplay file

--- src/gameplay/main_gameplay.py
+++ src/gameplay/main_gameplay.py
@@ -11,6 +11,10 @@ notes
 """
 
 
+#type hinting
+from typing import List, Dict
+
+
 
 #modules
 from tcod import *
@@ -23,7 +27,101 @@ import assets.color_tokens as color_tokens
 
 
 
+
+
+#classes
+class Element:
+    """Game element class.\n
+    Serves as the highest base class for all custom game elements."""
+
+    def __init__(self, name:str, tile:str) -> None:
+        self.name:str = name
+        self.tile:str = tile
+
+    def __repr__(self) -> str:
+        return f"{self.tile} : {self.name}"
+
+
+
+class Entity(Element):
+    """Entities elements class.\n
+    Inherits from the `Element` base class.\n
+    Serves as a base class for entities (`Entity` class objects)."""
+
+    def __init__(self, name:str, tile:str, adjectives_list:List[str], hp:int, max_hp:int) -> None:
+        super().__init__(name, tile)
+        self.adjectives:List[str] = adjectives_list
+        self.hp:int = hp
+        self.max_hp:int = max_hp
+
+    def move(self):
+        raise NotImplementedError()
+
+    def attack(self):
+        raise NotImplementedError()
+
+
+
+class Player(Entity):
+    """Player character object class.\n
+    Inherits from the `Entity` base class.\n
+    Inherits from interfaces (base classes) for its player-selected character class."""
+
+    def __init__(self, name: str, tile: str, adjectives_list: List[str], hp: int, max_hp: int) -> None:
+        super().__init__(name, tile, adjectives_list, hp, max_hp)
+
+        self.race:str = "SAMPLE_RACE"
+        self.name = "SAMPLE_NAME"               #TODO: set name to randomly-assigned one
+        self.gender = None                      #TODO: implement gender once text input has been fixed
+
+        self.char_class:str = "SAMPLE_CLASS"    #TODO: set class to selected one
+        self.traits:Dict[str, int] = {
+            "CON":10,
+            "DEX":10,
+            "INT":10,
+            "STR":10,
+            "WIS":10,
+        }
+
+
+        self.adjectives = [
+            "ambitious",
+            "bold", "brave",
+            "daring", "desperate",
+            "famous", "fortunate",
+            "great",
+            "intrepid",
+            "lawless",
+            "reckless",
+            "successful",
+            "unfortunate", "unscrupulous",
+            "wild"
+        ]
+
+        self.level:int = 1; #§ default value | TODO: allow to be set to 0 with debug
+
+
+    def move(self):
+        raise NotImplementedError()
+
+
+    def level_up(self):
+        raise NotImplementedError()
+
+
+
+
+
 #functions
+def create_player_object(player_object:Player, player_data:dict):
+    """Creates the `player_object` instance used during gameplay using preemptively saved `player_data`.\n
+    Returns ??? (`???`)."""
+    pass
+
+
+
+
+
 def main_gameplay(main_context:context.Context, main_console:Console, screen_width:int, screen_height:int):
     """The main gameplay function.\n
     Creates the main `context`, `terminal` and `console` for the main gameplay section.\n
